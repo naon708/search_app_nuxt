58746a2c13fb83cec25407390669016f21a1ccd6
naon708 <naoya_nis@icloud.com>
2022-02-26 18:30:08 +0900

    Add: display Variations

--- pages/variations.vue
+++ pages/variations.vue
@@ -1,19 +1,32 @@
 <template>
   <div>
-    <h3>ヴァリエーション</h3>
-    <div v-for="(variation, index) of variations" :key="index">
-      {{ variation.japanese_name }} / {{ variation.universal_name }}
+    <h3>コンクールの課題曲</h3>
+    <div v-for="program of variation_programs" :key="program.id">
+      {{ program.name }}
+      <div v-for="variation of variations" :key="variation.id" class="ml-4">
+        <v-card
+          class="mb-4 pl-4"
+          :href="forSearch(variation.universal_name)"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div v-if="program.name === variation.program_name">
+            {{ variation.japanese_name }}
+          </div>
+        </v-card>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getFirestore, collection, getDocs, query, orderBy } from "firebase/firestore";
 
 export default {
   data() {
     return {
-      variations: []
+      variations: [],
+      variation_programs: []
     }
   },
   async created() {
@@ -25,6 +38,24 @@ export default {
       });
     } catch (e) {
       console.error("Error:", e);
+    }
+    try {
+      const db = getFirestore(this.$firebaseApp);
+      const variationProgramsRef = collection(db, "variation_programs");
+      const sortQuery = query(variationProgramsRef, orderBy("id"));
+
+      const querySnapshot = await getDocs(sortQuery);
+      querySnapshot.forEach((doc) => {
+        this.variation_programs.push(doc.data());
+        console.log(this.variation_programs);
+      });
+    } catch (e) {
+      console.error("Error:", e);
+    }
+  },
+  methods: {
+    forSearch(word) {
+      return `https://www.youtube.com/results?search_query=ballet+${word}`
     }
   }
 }
