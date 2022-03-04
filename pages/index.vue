2d1bb178672d8b223bf3b62dfb03b862ccdd3f84
naon708 <naoya_nis@icloud.com>
2022-03-04 14:25:10 +0900

    Fix: layout on top page

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <v-card class="mb-4 pl-4" nuxt to="scenes">
+    <v-card class="ma-2 d-flex align-center justify-center" nuxt to="scenes" height="20vh">
       演目から好きなシーンを探す
     </v-card>
-    <v-card class="mb-4 pl-4" nuxt to="variations">
+    <v-card class="ma-2 d-flex align-center justify-center" nuxt to="variations" height="20vh">
       コンクールの課題曲
     </v-card>
-    <v-card class="mb-4 pl-4" nuxt to="steps">
+    <v-card class="ma-2 d-flex align-center justify-center" nuxt to="steps" height="20vh">
       パ / ステップを調べる
     </v-card>
-    <v-card class="mb-4 pl-4" nuxt to="dancers">
+    <v-card class="ma-2 d-flex align-center justify-center" nuxt to="dancers" height="20vh">
       ダンサーリスト
     </v-card>
   </div>
--- pages/variations.vue
+++ pages/variations.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h3>コンクールの課題曲</h3>
-    <div v-for="program of variation_programs" :key="program.id" class="text-center">
+    <div v-for="program of variation_programs" :key="program.id">
       {{ program.name }}
       <div v-for="variation of variations" :key="variation.title" class="ml-4">
         <v-card
