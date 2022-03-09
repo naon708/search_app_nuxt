2f8191af84a1fd67884b634cd7c931aac1095dbd
naon708 <naoya_nis@icloud.com>
2022-03-09 15:32:03 +0900

    Add: module dotenv

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 
 // This app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCDDTayLbzyHbo7HLc1Y80WxZrsXHvAxDA",
-  authDomain: "search-app-40e51.firebaseapp.com",
-  projectId: "search-app-40e51",
-  storageBucket: "search-app-40e51.appspot.com",
-  messagingSenderId: "1036374188691",
-  appId: "1:1036374188691:web:fffc102d90d78619d5e366"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
