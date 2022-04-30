0cdc3b964f11337c728c8a7b9f30b39c5de24785
naon708 <naoya_nis@icloud.com>
2022-04-30 19:47:03 +0900

    Add: メンテナンスモード

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <v-alert dense text type="success">
+      4/27に新バージョンをリリースしましたが、現在メンテナンス中のためこちらをご利用ください。
+    </v-alert>
+
     <v-card class="ma-2 d-flex align-center justify-center text--secondary" nuxt to="programs" height="20vh">
       バレエの演目を検索
     </v-card>
